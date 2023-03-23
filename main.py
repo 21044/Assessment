365385440f59874301feb252496ed9ecb3e0aa0a
Michael Hayes <126822012+21044@users.noreply.github.com>
2023-03-23 20:49:05 +0000

    commented certain areas of code, added all of the questions and answers

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import time
 amount_correct = 0
 
 # line 5 and 6 creates a list to store the questions and answers respectively
-q = ["Which statement do you use to output a message to the screen?", "q2", "q3", "q4", "q5"]
-a = ["print", "a2", "a3", "a4", "a5"]
+q = ["What is the name of the statement that you use to output a message to the screen?", "True or False, a function is used to hold multiple pieces of information such as strings, numbers and more.", "What is the name of the thing that IS used to store multiple pieces of information?", "Name the type of loop that will repeat until a value is changed.", 'True or False, an "if" statement does not need to have an "else" after it.']
+a = ["print", "false", "list", "while", "true"]
 
 # a function to compare the users inputs and the answers to the questions
 def compare(inp, ans):
   if inp == ans:
     time.sleep(0.5)
     print("Correct!")
+    # global makes the variable that comes after work outside of the function
     global amount_correct
     amount_correct += 1
   else:
     time.sleep(0.5)
     print("Wrong")
-    
 
 print("Hey there!")
 # "time.sleep()" will make the program wait before doing the next step
 time.sleep(0.5)
 print("Welcome to my quiz.")
+# print("\n") skips a line
 print("\n")
 time.sleep(1)
 # asks the user for their name and will store it in a variable
@@ -45,11 +46,13 @@ time.sleep(0.5)
 # "counter" will go up each time the loop runs through, after "counter" is above the amount of questions it will stop looping
 counter = 0
 while counter <= len(q) - 1:
-  # asks the user a question, each time the loop runs through the number of the list increases which asks a new question
+  # asks the user a question, each time the loop runs through, the number of the list increases which asks a new question
   print(q[counter])
   print("\n")
   time.sleep(0.5)
+  # asks the user for their input and converts it to lowercase so it ignores capitalization
   user_answer = input("Answer: ").lower()
+  # checks if the users answer is using only letters and asks the user again if it isnt.
   if user_answer.isalpha() == False:
     user_answer = input("Please do not use special characters, please try again. ")
   # calls the function "compare()"
